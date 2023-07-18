47b7dd153cc299e98145eea8f29bc5034c0b00f3
Mauro <maurovera069@gmail.com>
2023-07-18 09:30:33 -0300

    Agregue el endpoint para subir imagenes con sus respectivas dependencias

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const upload = require("./libs/storage");
+const app = express();
+const port = 3000;
 
 // Ruta principal
-app.get('/', (req, res) => {
-  res.send('¡Hola mundo desde la API!')
-})
+app.post(
+  "/upload-img",
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  (req, res) => {
+    const { image } = req.files;
+    console.log({ file: req.file });
+
+    res.send("¡Hola mundo desde la API!");
+  }
+);
 
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`La API está escuchando en http://localhost:${port}`)
-})
+  console.log(`La API está escuchando en http://localhost:${port}`);
+});
